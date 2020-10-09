ddfe2873b2e11f11fee283b5572fd3f6e9bc0b66
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2020-10-09 12:29:24 +0300

    feat: perform task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let arr = str.split('');
+ if (arr.length % 2 !== 0) return false;
+ if (arr) {
+   for (let i = 0; i <= arr.length - 1; i++) {
+   bracketsConfig.forEach( item => {
+     if (arr[i] === item[0] && arr[i+1] === item[1]) {
+       arr.splice(i, 2);
+       i = -1;
+     }
+   })
+    
+ } 
+ }
+ console.log(arr);
+ return arr.length === 0;
 }
